49c90581602a69115e1206fe701cc8d3db2af620
VAIQUE Benjamin <bevaique@cwf.fr>
2026-05-28 09:55:00 +0200

    feat: implement custom exception classes for chess game validation logic

---
+++ src/Exception/InvalidMoveException.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/ChessException.php';
+
+class InvalidMoveException extends ChessException
+{
+}
---
+++ src/Exception/WrongTurnException.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/ChessException.php';
+
+class WrongTurnException extends ChessException
+{
+}
